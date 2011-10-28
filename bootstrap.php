f407a7b5d8c4460c451a9ef9dcaffed124a33e60
Chris Hartjes <chartjes@yggdrasil.local>
2011-10-27 21:40:50 -0400

    Refactored to not double-define support constants

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
 // set some directory names that we will need
-define('APP_ROOT', __DIR__ . '/');
-define('LIB_ROOT', APP_ROOT . 'lib/');
+
+if (!defined('APP_ROOT')) {
+    define('APP_ROOT', __DIR__ . '/');
+}
+
+if (!defined('LIB_ROOT')) {
+    define('LIB_ROOT', APP_ROOT . 'lib/');
+}
 
 // include our autoloader
 include LIB_ROOT . 'psr0.autoloader.php';
