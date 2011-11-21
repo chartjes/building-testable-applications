53d17b5d82db939c5dadfa806d4907967b3f8c1d
Chris Hartjes <chartjes@yggdrasil.cgocable.net>
2011-11-20 22:02:15 -0500

    Removed inflect() method as it was no longer needed

--- lib/ibl/BaseModel.php
+++ lib/ibl/BaseModel.php
@@ -18,23 +18,13 @@ class BaseModel
     {
         if (preg_match('/^(get|set)(\w+)/', $name, $match) && $attribute = $this->validateAttribute($match[2])) {
             if ($match[1] == 'get') {
-                $newAttribute = $this->inflect($attribute);
-                return $this->$newAttribute;
+                return $this->$attribute;
             } else {
-                $newAttribute = $this->inflect($attribute);
-                $this->$newAttribute = $args[0];
+                $this->$attribute = $args[0];
             }
         } else {
             throw new \Exception('Call to undefined ' . get_class($this) . '::' . $name . '()'); 
         }
-    }
-
-    protected function inflect($str) {
-        $str[0] = strtolower($str[0]);
-
-        return preg_replace_callback("/([A-Z])/", function($c) {
-            return strtolower($c[1]); 
-        }, $str);
     }
     
     protected function validateAttribute($name) {
@@ -44,9 +34,9 @@ class BaseModel
 
         if (in_array($fieldName, array_keys(get_class_vars(get_class($this))))) {
             return $fieldName; 
-        } else {
-            echo "couldn't find {$fieldName}\n"; 
         }
+
+        return false;
     }
 }
 
