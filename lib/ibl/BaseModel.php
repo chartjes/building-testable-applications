809fd83abbd6537b159b889f6819da261b7c6572
Chris Hartjes <chartjes@yggdrasil.cgocable.net>
2011-11-20 22:10:33 -0500

    Some refactoring to meet Zend coding standard

--- lib/ibl/BaseModel.php
+++ lib/ibl/BaseModel.php
@@ -16,24 +16,32 @@ class BaseModel
 
     public function __call($name, $args)
     {
-        if (preg_match('/^(get|set)(\w+)/', $name, $match) && $attribute = $this->validateAttribute($match[2])) {
+        if (preg_match('/^(get|set)(\w+)/', $name, $match) 
+            && $attribute = $this->validateAttribute($match[2])) {
             if ($match[1] == 'get') {
                 return $this->$attribute;
             } else {
                 $this->$attribute = $args[0];
             }
         } else {
-            throw new \Exception('Call to undefined ' . get_class($this) . '::' . $name . '()'); 
+            throw new \Exception(
+                'Call to undefined ' . 
+                get_class($this) . 
+                '::' . 
+                $name . 
+                '()'
+            ); 
         }
     }
     
-    protected function validateAttribute($name) {
+    protected function validateAttribute($name) 
+    {
         // Convert first alphanumerica character of the strong to lowercase 
-        $fieldName = '_' . $name;
-        $fieldName{1} = strtolower($fieldName{1}); 
+        $field = '_' . $name;
+        $field{1} = strtolower($field{1}); 
 
-        if (in_array($fieldName, array_keys(get_class_vars(get_class($this))))) {
-            return $fieldName; 
+        if (in_array($field, array_keys(get_class_vars(get_class($this))))) {
+            return $field; 
         }
 
         return false;
