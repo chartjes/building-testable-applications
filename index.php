73fa2a7ba01005b705d3a58e7e44eaab62812c60
Chris Hartjes <chartjes@yggdrasil.(none)>
2011-12-20 17:11:51 -0500

    Added in rotation information to the main page

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ include 'bootstrap.php';
 // Load data that we will need for the front page
 $gameMapper = new \IBL\GameMapper($container['db_connection']);
 $franchiseMapper = new \IBL\FranchiseMapper($container['db_connection']);
+$rotationMapper = new \IBL\RotationMapper($container['db_connection']);
+
 $games = $gameMapper->findAll();
 $franchises = $franchiseMapper->findAll();
 $standings = new \IBL\Standings($games, $franchises);
@@ -15,13 +17,32 @@ $currentResults = $gameMapper->generateResults(
     $franchises
 );
 
+/**
+ * If we don't have any rotations for the current week, make sure to grab
+ * rotations for the previous week
+ */
+$rotations = $rotationMapper->findByWeek($currentWeek);
+$rotationWeek = $currentWeek;
+
+if (count($rotations) == 0) {
+    $rotations = $rotationMapper->findByWeek($currentWeek - 1);
+    $rotationWeek = $currentWeek - 1;
+}
+
+$currentRotations = $rotationMapper->generateRotations(
+    $rotations,
+    $franchises
+);
+
 // Display the data
 echo $twig->render(
     'index.html', 
     array(
-        'standings' => $regularStandings, 
-        'franchises' => $franchises,
         'currentWeek' => $currentWeek,
-        'currentResults' => $currentResults
+        'currentResults' => $currentResults,
+        'currentRotations' => $currentRotations,
+        'franchises' => $franchises,
+        'rotationWeek' => $rotationWeek,
+        'standings' => $regularStandings, 
     )
 );
